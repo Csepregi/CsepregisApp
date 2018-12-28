d23cdf06154f724428c30af72f114360e6eee7ec
Gabor Csepregi <csepregigabor02@gmail.com>
2018-12-28 16:38:59 +0100

    add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes    = require("./routes/comments"),
 
 
 
-mongoose.connect("mongodb://localhost: 27017/yelp_camp_v3", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost: 27017/yelp_camp_v3", {useNewUrlParser: true});
+mongoose.connect("mongodb://gabor:Hangfive2019@ds026658.mlab.com:26658/yelpcamp", {useNewUrlParser: true});
+//mongodb://gabor:Hangfive2019@ds026658.mlab.com:26658/yelpcamp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public")); //dirname refers to the directory that the script is running
